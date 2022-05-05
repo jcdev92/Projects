5c5ac3b9c6ef9de52680b64e849f0c4b3e130c7b
jcdev <jcdeveloper92@gmail.com>
2022-05-05 17:13:52 -0400

    Spider Quotes Scraper v1.0

--- quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -15,25 +15,12 @@ class quotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        print('*'*10)
-        print('\n\n\n')
-        ## print(response.status, response.headers)
         title = response.xpath('//h1/a/text() ').get()
-        print(f'Titulo: {title}')
-        print('\n\n')
-
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        print('Citas: ')
-        for quote in quotes:
-            print(f'- {quote}')
-        print('\n\n')
-
         ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
-        print('ten tags: ')
-        for tag in ten_tags:
-            print(f'- {tag}')
-        print('\n\n')
-        
-        print('\n\n\n')
-        print('*'*10)
-        
+
+        yield {
+            'title': title,
+            'quotes' : quotes,
+            'ten_tags': ten_tags
+        }
