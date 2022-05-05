6a6a4627173a7cb518fec0330536bed89f952a6d
jcdev <jcdeveloper92@gmail.com>
2022-05-05 18:04:47 -0400

    Spider Quotes Scraper v1.2 - back

--- quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes_2.py
+++ quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes_2.py
@@ -2,7 +2,7 @@ import scrapy
 # Title
 ## h1/a/text() 
 
-# Quotes
+# Citas 
 ## //span[@class="text" and @itemprop="text"]/text()
 
 # Top Ten Tags
@@ -16,12 +16,20 @@ class quotesSpider(scrapy.Spider):
 
     def parse(self, response):
         title = response.xpath('//h1/a/text() ').get()
+        short = ""
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        for i in quotes:
+            short = i
+            yield short
+        short_tag = ""
         ten_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
-
+        for n in ten_tags:
+            short_tag = n
+            yield short_tag
         
         yield {
             'title': title,
-            'quote' : [short for short in quotes],
-            'ten_tags': [short_tag for short_tag in ten_tags],
+            'quote' : short,
+            'ten_tags': short_tag
         }
+        
