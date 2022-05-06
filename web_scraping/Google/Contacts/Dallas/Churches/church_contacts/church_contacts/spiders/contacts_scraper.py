772b5ee5ba28a43467649176bb9c4ec7712b1cea
jcdev <jcdeveloper92@gmail.com>
2022-05-06 14:06:49 -0400

    Churches Dallas Contacts Scrapper - Beta 0.1.2

--- web_scraping/Google/Contacts/Dallas/Churches/church_contacts/church_contacts/spiders/contacts_scraper.py
+++ web_scraping/Google/Contacts/Dallas/Churches/church_contacts/church_contacts/spiders/contacts_scraper.py
@@ -19,8 +19,8 @@ class contactsSpider(scrapy.Spider):
     start_urls = [
         'https://www.google.com/search?q=75230%20churches&oq=75&aqs=chrome.0.69i59j69i57j69i59j0i512j46i512j69i65j69i60l2.1392j0j7&sourceid=chrome&ie=UTF-8&tbs=lf:1,lf_ui:2&tbm=lcl&sxsrf=ALiCzsYr1_AnbPjV1l7413vSWnbN8lV4oQ:1651860160914&rflfq=1&num=10&rldimm=5676161483414474647&lqi=Cg43NTIzMCBjaHVyY2hlc1oUIg43NTIzMCBjaHVyY2hlcyoCCAOSARBtZXRob2Rpc3RfY2h1cmNoqgEQEAEqDCIIY2h1cmNoZXMoAA&ved=2ahUKEwjxhvDQusv3AhWQlmoFHRiQBG0QvS56BAgEEAE&sa=X&rlst=f#rlfi=hd:;si:5676161483414474647,l,Cg43NTIzMCBjaHVyY2hlc1oUIg43NTIzMCBjaHVyY2hlcyoCCAOSARBtZXRob2Rpc3RfY2h1cmNoqgEQEAEqDCIIY2h1cmNoZXMoAA;mv:[[32.9260996,-96.7680381],[32.8625549,-96.8225437]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:2'
     ]
-    if os.path.exists('contacts.csc'):
-        os.remove("contacts.csc")
+#    if os.path.exists('contacts.csc'):
+#        os.remove("contacts.csc")
     
     custom_settings = {
         "FEEDS":{"contacts.css":{"format":"css"}}
