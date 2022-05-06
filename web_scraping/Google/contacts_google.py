21599b6a97d408f301c1f35b270593ea4f709857
jcdev <jcdeveloper92@gmail.com>
2022-05-06 16:27:20 -0400

    Some testes

--- web_scraping/Google/contacts_google.py
+++ web_scraping/Google/contacts_google.py
@@ -1,7 +1,7 @@
 import time
 from selenium import webdriver
 
-drive = webdriver.Chrome(executable_path="Ubuntu\home\gordo\jmcg_dev\Projects\web_scraping\Google\chromedriver")
+drive = webdriver.Chrome(executable_path="/home/gordo/jmcg_dev/Projects/web_scraping/Google/chromedriver_linux64/chromedriver")
 
 time.sleep(5)
 drive.quit()
