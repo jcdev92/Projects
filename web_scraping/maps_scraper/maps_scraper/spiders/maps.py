fac635073173c35084cd5418c024c82c391d8960
jcdev <jcdeveloper92@gmail.com>
2022-05-08 17:28:18 -0400

    maps srcapper 1.0

--- web_scraping/maps_scraper/maps_scraper/spiders/maps.py
+++ web_scraping/maps_scraper/maps_scraper/spiders/maps.py
@@ -1,44 +1,33 @@
 import scrapy
 import os
 
-# Business Title
-## '//h2[@class="qrShPb kno-ecr-pt PZPZlf q8U8x PPT5v hNKfZe"]/span/text()'
+# //div[2]/div/a[1]/div/div/div[1]
+# //div[2]/div/a[1]/div/div/div[3]
 
-# Direction
-## PATH 1  -----  '//*[@id="akp_tsuid10"]//div[2]/div/div/span[2]/text()'
-## PATH 2  -----  '//*[@id="akp_tsuid10"]//div[3]/div/div[2]/div/div/span[2]/text()'
+class mapsSpider(scrapy.Spider):
+    name = 'maps'
+    allowed_domains = ["https://www.google.com/?hl=es"]
+    start_urls = ['https://www.google.com/search?tbs=lf:1,lf_ui:2&tbm=lcl&sxsrf=ALiCzsY6NwHEcmizK569Vy1KLWEQdeF3fA:1652042344804&q=75230+churches&rflfq=1&num=10&sa=X&ved=2ahUKEwjVu_ao4dD3AhUhRjABHTdiAmQQjGp6BAgeEAE&biw=819&bih=622&dpr=1.49#rlfi=hd:;si:;mv:[[32.9260996,-96.7680381],[32.8625549,-96.8225437]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:2']
 
-# Phone Number 
-## response.xpath('//a[@data-dtype="d3ph"]//span/text()').get()
-
-# Website URL 
-## response.xpath('//a[@class="ab_button CL9Uqc"]/@href').get()
-
-class contactsSpider(scrapy.Spider):
-    name = 'churches75230'
-    start_urls = [
-        'https://www.google.com/search?q=75230%20churches'
-    ]
 #    if os.path.exists('contacts.csc'):
 #        os.remove("contacts.csc")
-    
-    custom_settings = {
-        "FEEDS":{"contacts.css":{"format":"css"}}
-    }
 
     def parse(self, response):
-        name = response.xpath('//h2[@class="qrShPb kno-ecr-pt PZPZlf q8U8x PPT5v hNKfZe"]/span/text()').get()
-        direction = response.xpath('//span[@class="LrzXr"]/text()').get()
-        number = response.xpath('//a[@data-dtype="d3ph"]//span/text()').get()
-        website = response.xpath('//a[@class="ab_button CL9Uqc"]/@href').get()
         
-        yield {
-            'organitation': name,
-            'direction': direction,
-            'number': number,
-            'website': website
+        name = response.xpath('//a[1]/div/div/div[1]/span/text()').getall()
+        direction = response.xpath('//a[1]/div/div/div[3]/text()').getall()
+#        zone = response.xpath('//div[@class="locality"]/text()').getall()
+#        number = response.xpath('//div[@class="phones phone primary"]/text()').getall()
+        website = response.xpath('//div[1]/div/a[2]/@href').getall() and response.xpath('//div[2]/div/a[2]/@href').getall() and response.xpath('//div[3]/div/a[2]/@href').getall()       
+        for i in range(len(name)):
+            yield {
+            'organitation': name[i],
+            'direction': direction[i],
+#            'zone': zone[i],
+#            'number': number[i],
+            'website': website[i],
         }
 
-        next_page_button_link = response.xpath('//a[@class="fl"]/@href').get()
+        next_page_button_link = response.xpath('//td[3]/a/@href').get()
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse)       
+            yield response.follow(next_page_button_link, callback=self.parse)     
