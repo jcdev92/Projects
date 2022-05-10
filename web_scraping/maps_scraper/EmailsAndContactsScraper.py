431ffff59382dfc74d89d39cd0e0b3b8592c0a93
jcdev <jcdeveloper92@gmail.com>
2022-05-10 12:42:44 -0400

    Google Maps Scrapper 1.4

--- web_scraping/maps_scraper/EmailsAndContactsScraper.py
+++ web_scraping/maps_scraper/EmailsAndContactsScraper.py
@@ -4,7 +4,20 @@ from outscraper import ApiClient
 api_client = ApiClient(api_key='Z29vZ2xlLW9hdXRoMnwxMTE0OTA0MzI1MDM2NTczMTMwODl8NDc3MzA4N2MxNw')
 
 # Search for businesses in specific locations:
-result = api_client.emails_and_contacts(['https://medicalcityhealthcare.com/'])
+result = api_client.emails_and_contacts(['http://www.renewalmedicalcenters.com/'])
 
-with open('email_and_contact.json', 'w') as file:
+with open('renewalmedicalcenters.json', 'w') as file:
     json.dump(result, file, indent=4)
+
+
+#  
+#  
+#  
+#  
+#  
+# 
+#  
+#  
+#  
+#  
+
--- web_scraping/maps_scraper/maps_scraper.py
+++ web_scraping/maps_scraper/maps_scraper.py
@@ -4,7 +4,7 @@ from outscraper import ApiClient
 api_client = ApiClient(api_key='Z29vZ2xlLW9hdXRoMnwxMTE0OTA0MzI1MDM2NTczMTMwODl8NDc3MzA4N2MxNw')
 
 # Search for businesses in specific locations:
-result = api_client.google_maps_search('medical centers dallas', limit=200, language='es')
+result = api_client.google_maps_search('dallas restaurants', limit=200, language='es')
 
 with open('maps_result.json', 'w') as file:
     json.dump(result, file, indent=4)
