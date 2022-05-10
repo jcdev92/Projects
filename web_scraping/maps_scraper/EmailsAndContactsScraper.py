cacf42bf5c6aa21e11a00bc88e53405d348e0adc
jcdev <jcdeveloper92@gmail.com>
2022-05-09 23:02:27 -0400

    xyz

--- web_scraping/maps_scraper/EmailsAndContactsScraper.py
+++ web_scraping/maps_scraper/EmailsAndContactsScraper.py
@@ -4,7 +4,7 @@ from outscraper import ApiClient
 api_client = ApiClient(api_key='Z29vZ2xlLW9hdXRoMnwxMTE0OTA0MzI1MDM2NTczMTMwODl8NDc3MzA4N2MxNw')
 
 # Search for businesses in specific locations:
-result = api_client.emails_and_contacts(['www.metroanesthesia.com'])
+result = api_client.emails_and_contacts(['https://medicalcityhealthcare.com/'])
 
-with open('maps_result.json', 'w') as file:
+with open('email_and_contact.json', 'w') as file:
     json.dump(result, file, indent=4)
