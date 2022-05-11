92414467cce86648857ea7ac183c018fe75111b9
jcdev <jcdeveloper92@gmail.com>
2022-05-11 12:31:46 -0400

    Google Maps Scrapper 1.4

--- web_scraping/maps_scraper/EmailsAndContactsScraper.py
+++ web_scraping/maps_scraper/EmailsAndContactsScraper.py
@@ -3,21 +3,20 @@ from outscraper import ApiClient
 
 api_client = ApiClient(api_key='Z29vZ2xlLW9hdXRoMnwxMTE0OTA0MzI1MDM2NTczMTMwODl8NDc3MzA4N2MxNw')
 
+webs = [
+] 
+
+
+webs_2 = [
+
+]
+
+webs_3 = [
+   
+]
+
 # Search for businesses in specific locations:
-result = api_client.emails_and_contacts(['http://www.renewalmedicalcenters.com/'])
+result = api_client.emails_and_contacts(webs)
 
-with open('/home/gordo/jmcg_dev/Projects/web_scraping/maps_scraper/json_files/renewalmedicalcenters.json', 'w') as file:
+with open('/home/gordo/jmcg_dev/Projects/web_scraping/maps_scraper/json_files/hoas_contacts.json', 'w') as file:
     json.dump(result, file, indent=4)
-
-
-#  
-#  
-#  
-#  
-#  
-# 
-#  
-#  
-#  
-#  
-
