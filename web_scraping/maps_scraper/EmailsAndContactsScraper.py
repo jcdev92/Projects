5f3287090056ba65859bd17afd7df7c022ab178e
jcdev <jcdeveloper92@gmail.com>
2022-05-11 21:44:09 -0400

    doing scrapes

--- web_scraping/maps_scraper/EmailsAndContactsScraper.py
+++ web_scraping/maps_scraper/EmailsAndContactsScraper.py
@@ -19,5 +19,5 @@ api_client = ApiClient(api_key='Z29vZ2xlLW9hdXRoMnwxMTE0OTA0MzI1MDM2NTczMTMwODl8
 # Search for businesses in specific locations:
 result = api_client.emails_and_contacts(webs)
 
-with open('/home/gordo/jmcg_dev/Projects/web_scraping/maps_scraper/json_files/ppty_mngt_contacts.json', 'w') as file:
+with open('/home/gordo/jmcg_dev/Projects/web_scraping/maps_scraper/json_files/contracts_contacts.json', 'w') as file:
     json.dump(result, file, indent=4)
