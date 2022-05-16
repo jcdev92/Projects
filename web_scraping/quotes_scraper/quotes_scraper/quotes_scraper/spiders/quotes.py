6a4c2150b53c6cd3bf5976bfec36e87e73819a0e
jcdev <jcdeveloper92@gmail.com>
2022-05-16 19:17:45 -0400

    agency project starting

--- web_scraping/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ web_scraping/quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -20,30 +20,40 @@ class quotesSpider(scrapy.Spider):
         'http://quotes.toscrape.com/'
     ]
 
-    # if os.path.exists('quotes.json'):
-    #     os.remove("quotes.json")
+    if os.path.exists('quotes.json'):
+         os.remove("quotes.json")
     
 
     custom_settings = {
-        "FEEDS":{"quotes.json":{"format":"json"}}
+        "FEEDS": {"quotes.json" : {"format":"json"}},
+        'CONCURRENT_REQUESTS': 24,
+        'MEMUSAGE_LIMIT_MB': 2024,
+        'MEMUSAGE_NOTIFY_MAIL': ['jcdeveloper92@gmail.com'],
+        'ROBOTSTXT_OBEY': False,
+        'USER_AGENT' : 'JCDEV',
+        'FEED_EXPORT_ENCODING': 'utf-8'
     }
 
     def parse_quotes(self, response, **kwargs):
         if kwargs:
             quotes = kwargs['quotes']
+            author = kwargs['author']
         quotes.extend(response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall())
+        author.extend(response.xpath('//small/text()').getall())
         next_page_button_link = response.xpath('//li[@class="next"]/a/@href').get()
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse_quotes, cb_kwargs={'quotes': quotes})
+            yield response.follow(next_page_button_link, callback=self.parse_quotes, cb_kwargs={'quotes': quotes, 'author': author})
         else: 
             yield {
                 'quotes' : quotes,
+                'author' : author
             }
     
 
     def parse(self, response):
         title = response.xpath('//h1/a/text() ').get()
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        author = response.xpath('//small/text()').getall()
         top_tags = response.xpath('//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
         
         top = getattr(self, 'top', None)
@@ -58,4 +68,4 @@ class quotesSpider(scrapy.Spider):
 
         next_page_button_link = response.xpath('//li[@class="next"]/a/@href').get()
         if next_page_button_link:
-            yield response.follow(next_page_button_link, callback=self.parse_quotes, cb_kwargs={'quotes': quotes})       
+            yield response.follow(next_page_button_link, callback=self.parse_quotes, cb_kwargs={'quotes': quotes, 'author': author})       
