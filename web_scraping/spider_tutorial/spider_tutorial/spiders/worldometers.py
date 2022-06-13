ee69b58efe9c45beb62f934401f69ba17973514b
jcdev92 <jcdeveloper92@gmail.com>
2022-06-13 12:33:35 -0400

    tutorial scrapy

--- web_scraping/spider_tutorial/spider_tutorial/spiders/worldometers.py
+++ web_scraping/spider_tutorial/spider_tutorial/spiders/worldometers.py
@@ -7,10 +7,30 @@ class WordldometersSpider(scrapy.Spider):
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        title = response.xpath('//h1/text()').get
-        countries = response.xpath('//td/a/text()').getall()
+        #title = response.xpath('//h1/text()').get
+        countries = response.xpath('//td/a/')
 
-        yield {
-            'title': title,
-            'countries': countries
-        }
+        for country in countries:
+            country_name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
+
+        yield response.follow(url=link, callback=self.parse_country, meta={'country': country_name})
+
+        # yield {
+        #     'title': title,
+        #     'countries': countries
+        # }
+
+    def parse_country(self, response):
+        country = response.request.meta = ['country']
+        # response.xpath('(//table[@class="table table-striped table-bordered table-hover table-condensed table-list"])[1]/tbody/tr')
+        rows = response.xpath("(//table[contains(@class, 'table')])[1]/tbody/tr")
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            pupulation = row.xpath(".//td[2]/strong/text()").get()
+
+            yield {
+                'Country' : country,
+                'Year': year,
+                'Population': population,
+            }
