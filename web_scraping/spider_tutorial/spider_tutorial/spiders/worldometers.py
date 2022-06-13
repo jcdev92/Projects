b6ee28ab36e1bba5ab99f5911dbe4db0a7b1575a
jcdev92 <jcdeveloper92@gmail.com>
2022-06-13 13:18:34 -0400

    scrapy library

--- web_scraping/spider_tutorial/spider_tutorial/spiders/worldometers.py
+++ web_scraping/spider_tutorial/spider_tutorial/spiders/worldometers.py
@@ -7,8 +7,8 @@ class WordldometersSpider(scrapy.Spider):
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        #title = response.xpath('//h1/text()').get
-        countries = response.xpath('//td/a/')
+        # title = response.xpath('//h1/text()').get
+        countries = response.xpath('//td/a')
 
         for country in countries:
             country_name = country.xpath(".//text()").get()
@@ -22,12 +22,12 @@ class WordldometersSpider(scrapy.Spider):
         # }
 
     def parse_country(self, response):
-        country = response.request.meta = ['country']
+        country = response.request.meta['country']
         # response.xpath('(//table[@class="table table-striped table-bordered table-hover table-condensed table-list"])[1]/tbody/tr')
-        rows = response.xpath("(//table[contains(@class, 'table')])[1]/tbody/tr")
+        rows = response.xpath("(//table[contains(@class,'table')])[1]/tbody/tr")
         for row in rows:
             year = row.xpath(".//td[1]/text()").get()
-            pupulation = row.xpath(".//td[2]/strong/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
 
             yield {
                 'Country' : country,
